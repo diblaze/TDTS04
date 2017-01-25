7f10948d0eaf9285a4d6ae76e8b40be63c426518
Denis I. Blazevic <denicevap@gmail.com>
2017-01-25 20:54:05 +0100

    bug with filter

--- RubyProxy/http_proxy.rb
+++ RubyProxy/http_proxy.rb
@@ -37,7 +37,7 @@ class HttpProxy
   end
 
 
-  def HandleRequest client
+  def HandleRequest(client)
     # What does client want to do?
     # EXAMPLE: GET http://www.w3.org/pub/WWW/TheProject.html HTTP/1.1
     request_line = client.readline
@@ -52,6 +52,7 @@ class HttpProxy
     url = request_line[/^\w+\s+(\S+)/, 1]
     version = request_line[/^\w+\s+\S+\s(HTTP\/1.\d)/, 1]
 
+
     #uri = URI.parse("http://www.google.se/")
     uri = URI.parse(url)
     p uri.host
@@ -61,19 +62,19 @@ class HttpProxy
     #modify?
 
     # Open connection to target server
-    target_server = TCPSocket.new(uri.host, 80)
+    target_server = TCPSocket.new(uri.host, uri.port)
     # Send request to target server
     puts "#{verb} #{uri.path} #{version}\r\n"
-    target_server.write("#{verb} #{uri.path} #{version}\r\n")
+    target_server.write("#{verb} #{uri.path} #{version}")
     #close the connection to target_server (maybe keep-alive to keep feeding data to client?)
-    #target_server.write("Connection: close\r\n\r\n")
+    target_server.write("Connection: close\r\n\r\n")
 
 
     #while (buffer = target_server.recv(2048))
     #  p buffer
     #end
     buffer = ''
-    while(tmp = target_server.gets)
+    while (tmp = target_server.gets)
       buffer += tmp
     end
     header, body = buffer.split("\r\n\r\n", 2)
@@ -82,11 +83,13 @@ class HttpProxy
     # Loop to get all data
 
 
-
     # Check if forbidden
-      # Redirect
-    # else send back data to client
-    client.write(buffer)
+    #if HasForbidden(buffer) #TODO: Check for version 1.0!!!!!!
+      #client.write("HTTP/1.1 301 Moved Permanently\r\nLocation: http://www.ida.liu.se/~TDTS04/labs/2011/ass2/error2.html\r\n\r\n")
+    #else
+      puts "YEA"
+      client.write(buffer)
+    #end
 
     # Close connections and sockets
     client.close
@@ -98,15 +101,20 @@ class HttpProxy
   end
 
   #HasForbidden Method
-    # Use Regex to see if forbidden words exists in given buffer
-      # Return True if has forbidden words
-    # Return False
+  # Use Regex to see if forbidden words exists in given buffer
+  # Return True if has forbidden words
+  # Return False
   #End HasForbidden
 
-
-
-
-
+  def HasForbidden(buffer)
+    forbidden_words = ["SpongeBob, Britney Spears, Paris Hilton, or Norrköping"]
+    forbidden_words.each do |word|
+      if buffer =~ /#{word}/
+        return true
+      end
+    end
+    return false
+  end
 
 end
 
